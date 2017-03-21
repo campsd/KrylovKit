667505a1c42da261c39eb3c5781eeee982d3eb25
campsd <daancamps@gmail.com>
2017-03-21 19:38:48 +0100

    cleaning-up repo

--- src/methods/common/CreateRotMat.m
+++
@@ -1,5 +0,0 @@
-function [Rot] = CreateRotMat(Grot)
-    % Creates 2x2 rotational matrix based on Grot which stores the first
-    % row
-    Rot = [Grot(1) Grot(2); -conj(Grot(2)) conj(Grot(1))];
-end
